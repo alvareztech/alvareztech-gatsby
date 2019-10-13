b5fd8e1ea2861f277197683499587d6377e2bc14
Daniel Alvarez <a3dany@gmail.com>
2019-10-13 18:50:10 -0400

    Toogle menu button action

--- src/components/header.js
+++ src/components/header.js
@@ -1,58 +1,78 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import logo from "../images/daniel-circle.png"
 
-const Header = ({ siteTitle }) => (
-  <header>
+class Header extends Component {
 
-    <nav className="navbar">
-      <div className="container">
-        <div className="navbar-brand">
-          <Link className="navbar-item" to="/">
-            <img src={logo} alt={siteTitle}/>
-          </Link>
-          <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-        <div id="navbarExampleTransparentExample" className="navbar-menu">
-          <div className="navbar-start">
-            <Link className="navbar-item" to="/contact">Contact</Link>
-            <Link className="navbar-item" to="/about">About</Link>
-          </div>
+  state = {
+    activeMenu: false,
+  }
 
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="field is-grouped">
-                <p className="control">
-                  <a className="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet"
-                     target="_blank"
-                     href="https://twitter.com/alvarez_tech">
+  toggleMenu = () => {
+    this.setState({
+      activeMenu: !this.state.activeMenu,
+    })
+  }
+
+  render () {
+    let { siteTitle } = this.props
+    return (
+      <header>
+
+        <nav className="navbar">
+          <div className="container">
+            <div className="navbar-brand">
+              <Link className="navbar-item" to="/">
+                <img src={logo} alt={siteTitle}/>
+              </Link>
+              <div className={`navbar-burger burger ${this.state.activeMenu ? "is-active" : ""}`}
+                   data-target="navbarExampleTransparentExample"
+                   onClick={this.toggleMenu}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+            <div id="navbarExampleTransparentExample"
+                 className={`navbar-menu ${this.state.activeMenu ? "is-active" : ""}`}>
+              <div className="navbar-start">
+                <Link className="navbar-item" to="/contact">Contact</Link>
+                <Link className="navbar-item" to="/about">About</Link>
+              </div>
+
+              <div className="navbar-end">
+                <div className="navbar-item">
+                  <div className="field is-grouped">
+                    <p className="control">
+                      <a className="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet"
+                         target="_blank"
+                         href="https://twitter.com/alvarez_tech">
                     <span>
                 Twitter
               </span>
-                  </a>
-                </p>
-                <p className="control">
-                  <a className="button is-primary"
-                     target="_blank"
-                     href="https://www.youtube.com/alvareztech">
-                    YouTube
-                  </a>
-                </p>
+                      </a>
+                    </p>
+                    <p className="control">
+                      <a className="button is-primary"
+                         target="_blank"
+                         href="https://www.youtube.com/alvareztech">
+                        YouTube
+                      </a>
+                    </p>
+                  </div>
+                </div>
+
               </div>
             </div>
-
           </div>
-        </div>
-      </div>
-    </nav>
+        </nav>
 
-  </header>
-)
+      </header>
+    )
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
